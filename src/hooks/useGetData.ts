72d58a83d68ac25f2056579e0f21f3be0452e335
Jonas Nilsson <nilssoncjonas@gmail.com>
2023-08-23 20:52:05 +0200

    added fake delay to axios and removed null from custom hook

--- src/hooks/useGetData.ts
+++ src/hooks/useGetData.ts
@@ -2,9 +2,9 @@ import * as TMDB_API from '../services/TMDB_API'
 import {useQuery} from "@tanstack/react-query";
 import {useState} from "react";
 
-const useGetData = <T>(initialQueryKey: string | null, initialEndpoint: string | null) => {
-    const [queryKey, setQueryKey] = useState<string | null>(initialQueryKey)
-    const [endpoint, setEndpoint] = useState<string | null>(initialEndpoint)
+const useGetData = <T>(initialQueryKey: string, initialEndpoint: string) => {
+    const [queryKey, setQueryKey] = useState<string>(initialQueryKey)
+    const [endpoint, setEndpoint] = useState<string>(initialEndpoint)
     const {data, isError, isLoading, isSuccess, refetch} = useQuery(
         [queryKey],
         () => TMDB_API.get<T>(endpoint)
--- src/services/TMDB_API.ts
+++ src/services/TMDB_API.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = 'https://api.themoviedb.org/3/'
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY
-
+const FAKE_DELAY = 1500
 const instance = axios.create({
 	baseURL: BASE_URL,
 	timeout: 10000,
@@ -11,7 +11,10 @@ const instance = axios.create({
 		'accept': 'application/json',
 	}
 })
-const get = async <T>(endpoint: string) => {
+export const get = async <T>(endpoint: string) => {
+	console.log(endpoint)
 	const res = await instance.get<T>(BASE_URL + endpoint + API_KEY)
+	// Simulate a delay
+	!!FAKE_DELAY && await new Promise((r) => setTimeout(r, FAKE_DELAY))
 	return res.data
 }

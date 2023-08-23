66b88725e968da488e6cc6f4399c3f7962c10990
Jonas Nilsson <nilssoncjonas@gmail.com>
2023-08-23 08:06:33 +0200

    setup for tanstack query and it's devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 import App from './App.tsx'
 
-
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			staleTime: 5 * 60 * 1000,
+			cacheTime: 60 * 60 * 1000
+		}
+	}
+})
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<App/>
+			<ReactQueryDevtools initialIsOpen={false} position={"bottom-left"}/>
+		</QueryClientProvider>
+	</React.StrictMode>,
 )

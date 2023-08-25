2ba64563d4dbc1f32f7bfb1b5059bea74ab562cd
Jonas Nilsson <nilssoncjonas@gmail.com>
2023-08-25 14:41:59 +0200

    pagnation and page searchparams are now working

--- src/hooks/useGetDataListPages.ts
+++ src/hooks/useGetDataListPages.ts
@@ -1,16 +1,6 @@
 import {useQuery} from "@tanstack/react-query";
 import * as TMDB_API from "../services/TMDB_API.ts";
-const useGetDataListPages = <T>(queryKey:( string |  number)[], resource: string, page: number) => {
-	const {data, isError, isLoading, isSuccess, refetch} = useQuery(
-		queryKey,
-		() => TMDB_API.getDataListWithPages<T>(resource, page)
-	)
-    return {
-			data,
-			isError,
-			isLoading,
-			isSuccess,
-			refetch
-		}
+const useGetDataListPages = <T>(queryKey: (string | number)[], resource: string, page: string) => {
+	return useQuery(queryKey,() => TMDB_API.getDataListWithPages<T>(resource, page))
 }
 export default useGetDataListPages
--- src/pages/Top_Rated.tsx
+++ src/pages/Top_Rated.tsx
@@ -1,51 +1,67 @@
-import {MovieRes} from "../types/index.types.ts";
-import C_MovieList from "../components/C_MovieList.tsx";
-import {useSearchParams} from "react-router-dom";
 import {useState} from "react";
+import {useLocation, useNavigate, useSearchParams} from "react-router-dom";
+// hooks
 import useGetDataListPages from "../hooks/useGetDataListPages.ts";
+// types
+import {MovieRes} from "../types/index.types.ts";
+// components
+import C_MovieList from "../components/C_MovieList.tsx";
+// style
 import Button from "react-bootstrap/Button";
 
 
 const Top_Rated = () => {
+	const navigate = useNavigate()
+	const location = useLocation()
+	const [searchParams, setSearchParams] = useSearchParams()
+	const pageParams = searchParams.get('page') ?? '1'
 
-    const [urlParams, setUrlParams] = useSearchParams()
-    const pageParams = urlParams.get('page')
+	// const [page, setPage] = useState(Number(pageParams) === 0 ? 1 : Number(pageParams))
 
-    const [page, setPage] = useState(Number(pageParams))
 
-    const {
-        data,
-        isLoading,
-        isSuccess,
-        isError,
-        refetch,
-    } = useGetDataListPages<MovieRes>(['movie/top_rated', page], 'movie/top_rated', page)
+	const {
+		data,
+		isSuccess,
+		isError,
+	} = useGetDataListPages<MovieRes>(['movie/top_rated', pageParams], 'movie/top_rated', pageParams)
 
-    const pageHandler = () => {
-        setPage(prevValue => prevValue + 1)
-        setUrlParams(`&page=${page}`)
-        // urlParams.set('page', page.toString())
-        console.log(urlParams)
-    }
 
-    return (
-        <>
-            <div className={'h2__wrap'}>
-                <h2>Top Rated Movies</h2>
-                <Button variant="outline-warning" onClick={() => pageHandler()}>page handler</Button>
-            </div>
-            {isSuccess && data ? (
-                <C_MovieList res={data.results}/>
-            ) : null}
-            {isError ? (
-                // TODO fix better error message
-                ' An error occurred...'
-            ) : null}
 
-            {/*	TODO pagnation*/}
+	const nextPage = () => {
+		// setPage(prevValue => prevValue + 1)
+		setSearchParams({page: String(Number(pageParams)+1)})
+	}
+	const prevPage = () => {
+		setSearchParams({page: String(Number(pageParams)-1)})
+	}
+	
+	// TODO queryClient?? set/get
+	return (
+		<>
+			<div className={'h2__wrap'}>
+				<h2>Top Rated Movies</h2>
+			</div>
 
-        </>
-    )
+			<div className={'m-3 px-4 d-flex justify-content-between'}>
+				<Button variant="outline-warning" onClick={prevPage}>prev page</Button>
+				<Button variant="outline-warning" onClick={nextPage}>next page</Button>
+			</div>
+			{isSuccess && data ? (
+					<div className={'text-center'}>
+						<p>{new Intl.NumberFormat('se-SV').format(data.total_results)} Result</p>
+						<p>Page {data.page} / {data.total_pages}</p>
+				<C_MovieList res={data.results}/>
+					</div>
+				) : null}
+			{isError ? (
+				// TODO fix better error message
+				' An error occurred...'
+			) : null}
+
+			{/*	TODO pagnation*/}
+
+		</>
+	)
 }
 
 export default Top_Rated

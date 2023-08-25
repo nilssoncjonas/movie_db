df1d9fa9c076e9124bdb9da488ad1b1820c02406
Jonas Nilsson <nilssoncjonas@gmail.com>
2023-08-25 15:19:59 +0200

    pagnation component completed,  to be included on pages

--- src/components/C_Pagination.tsx
+++ src/components/C_Pagination.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import Button from "react-bootstrap/Button";
 
 interface Props {
-
+	page: number
+	total_pages: number
+	hasPrevPage: boolean
+	hasNextPage: boolean
+	prevPage: () => void
+	nextPage: () => void
 }
 
-const C_Pagination: React.FC<Props> = ({}) => {
+const C_Pagination: React.FC<Props> = ({prevPage, nextPage, hasNextPage, hasPrevPage, page, total_pages }) => {
 
-    return (
-        <>
-        </>
-    ) 
+	return (
+		<>
+			<div className={'m-3 px-4 d-flex justify-content-between align-items-center'}>
+				<Button variant="outline-warning" disabled={!hasPrevPage} onClick={prevPage}>prev page</Button>
+				<p>Page {page} / {total_pages}</p>
+				<Button variant="outline-warning" disabled={!hasNextPage} onClick={nextPage}>next page</Button>
+			</div>
+		</>
+	)
 }
-    
+
 export default C_Pagination

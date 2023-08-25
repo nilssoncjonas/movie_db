df24aa772aa9fc589578ead6fbb9631fb2f56e61
Jonas Nilsson <nilssoncjonas@gmail.com>
2023-08-25 16:35:24 +0200

    removed files

--- src/hooks/useGetDataListPages.ts
+++
@@ -1,6 +0,0 @@
-import {useQuery} from "@tanstack/react-query";
-import * as TMDB_API from "../services/TMDB_API.ts";
-const useGetDataListPages = <T>(queryKey: (string | number)[], resource: string, page: string) => {
-	return useQuery(queryKey,() => TMDB_API.getDataListWithPages<T>(resource, page))
-}
-export default useGetDataListPages

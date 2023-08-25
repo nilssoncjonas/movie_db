cb149e3588b4ae6e25a1b4e28b3ef47695a0cbdc
Jonas Nilsson <nilssoncjonas@gmail.com>
2023-08-25 15:20:32 +0200

    Added pagination component

--- src/pages/Top_Rated.tsx
+++ src/pages/Top_Rated.tsx
@@ -1,23 +1,16 @@
-import {useState} from "react";
-import {useLocation, useNavigate, useSearchParams} from "react-router-dom";
+import {useSearchParams} from "react-router-dom";
 // hooks
 import useGetDataListPages from "../hooks/useGetDataListPages.ts";
 // types
 import {MovieRes} from "../types/index.types.ts";
 // components
 import C_MovieList from "../components/C_MovieList.tsx";
-// style
-import Button from "react-bootstrap/Button";
+import C_Pagination from "../components/C_Pagination.tsx";
 
 
 const Top_Rated = () => {
-	const navigate = useNavigate()
-	const location = useLocation()
 	const [searchParams, setSearchParams] = useSearchParams()
 	const pageParams = searchParams.get('page') ?? '1'
-
-	// const [page, setPage] = useState(Number(pageParams) === 0 ? 1 : Number(pageParams))
-
 
 	const {
 		data,
@@ -25,40 +18,53 @@ const Top_Rated = () => {
 		isError,
 	} = useGetDataListPages<MovieRes>(['movie/top_rated', pageParams], 'movie/top_rated', pageParams)
 
-
-
-	const nextPage = () => {
-		// setPage(prevValue => prevValue + 1)
-		setSearchParams({page: String(Number(pageParams)+1)})
-	}
-	const prevPage = () => {
-		setSearchParams({page: String(Number(pageParams)-1)})
-	}
-	
-	// TODO queryClient?? set/get
 	return (
 		<>
 			<div className={'h2__wrap'}>
 				<h2>Top Rated Movies</h2>
 			</div>
 
-			<div className={'m-3 px-4 d-flex justify-content-between'}>
-				<Button variant="outline-warning" onClick={prevPage}>prev page</Button>
-				<Button variant="outline-warning" onClick={nextPage}>next page</Button>
-			</div>
 			{isSuccess && data ? (
-					<div className={'text-center'}>
-						<p>{new Intl.NumberFormat('se-SV').format(data.total_results)} Result</p>
-						<p>Page {data.page} / {data.total_pages}</p>
-				<C_MovieList res={data.results}/>
-					</div>
-				) : null}
+				<div className={'text-center'}>
+					<p>{new Intl.NumberFormat('se-SV').format(data.total_results)} Result</p>
+					<C_Pagination
+						page={data.page}
+						total_pages={data.total_pages}
+						hasPrevPage={data.page > 1}
+						hasNextPage={data.page + 1 < data.total_pages}
+						prevPage={() => {
+							setSearchParams({page: String(Number(pageParams) - 1)})
+						}}
+						nextPage={() => {
+							setSearchParams({page: String(Number(pageParams) + 1)})
+						}}
+					/>
+					<C_MovieList res={data.results}/>
+				</div>
+			) : null}
 			{isError ? (
 				// TODO fix better error message
 				' An error occurred...'
 			) : null}
 
 			{/*	TODO pagnation*/}
+
+			{isSuccess ? (
+				<C_Pagination
+					page={data.page}
+					total_pages={data.total_pages}
+					hasPrevPage={data.page > 1}
+					hasNextPage={data.page + 1 < data.total_pages}
+					prevPage={() => {
+						setSearchParams({page: String(Number(pageParams) - 1)})
+						window.scrollTo({top: 0})
+					}}
+					nextPage={() => {
+						setSearchParams({page: String(Number(pageParams) + 1)})
+						window.scrollTo({top: 0})
+					}}
+				/>
+			): null }
 
 		</>
 	)

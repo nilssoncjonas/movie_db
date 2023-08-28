7465ea8ad6eefccfafe2b8e04ccae13f1503a676
Jonas Nilsson <nilssoncjonas@gmail.com>
2023-08-28 15:27:32 +0200

    minor style adjustment to footer

--- src/components/C_Footer.tsx
+++ src/components/C_Footer.tsx
@@ -23,11 +23,13 @@ const C_Footer = () => {
                     <p className={'JN__logo'}><a href={'https://www.jonasnilsson.dev'}><img
                         src={'https://www.jonasnilsson.dev/img/svg/jn_logo.svg'} alt="Jonas Nilsson"/> <span
                         id={'JN__link'}>jonasnilsson.dev</span></a></p>
-                    <hr/>
-                    <p><a href={'https://www.themoviedb.org/'}><img
-                        src={'https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg'}
-                        alt="The Movie DB . org"/></a></p>
+                    {/*<hr/>*/}
+
                     <p><a href={'https://developer.themoviedb.org/docs'}>All data provided by <span>TMDB-API</span></a>
+                    <a href={'https://www.themoviedb.org/'}>
+                        <img className={'TMDB__logo'}
+                        src={'https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg'}
+                        alt="The Movie DB . org"/></a>
                     </p>
 
                 </div>

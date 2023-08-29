2da8c47f20b809287a36991bd48a35a8d71a9773
Jonas Nilsson <nilssoncjonas@gmail.com>
2023-08-29 22:21:25 +0200

    updated endpoint

--- src/hooks/useGetNowPlaying.ts
+++ src/hooks/useGetNowPlaying.ts
@@ -2,8 +2,8 @@ import useGetData from "./useGetData.ts";
 import { NowPlayingRes } from "../types/index.types.ts";
 
 const useGetNowPlaying = (pageParams: string) => {
-	return useGetData<NowPlayingRes>(['movie/now_playing/', pageParams], `movie/now_playing?page=${pageParams}&region=US`)
-
+	return useGetData<NowPlayingRes>(['movie/now_playing/', pageParams], `movie/now_playing?language=en-US&page=${pageParams}`)
+	
 }
 
 export default useGetNowPlaying

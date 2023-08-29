202275022752c7794ea3d7e5d96f75dbe69766d7
Jonas Nilsson <nilssoncjonas@gmail.com>
2023-08-29 18:33:06 +0200

    optimized single movie, created components for cast, similar movies and loading placeholder

---
+++ src/components/C_MoviePage_Placeholder.tsx
@@ -0,0 +1,29 @@
+const C_MoviePage_Placeholder = () => {
+	
+	return (
+		<>
+			<div className={'single__movie__placeholder__wrap'}>
+				<span className="image placeholder placeholder-wave bg-warning"></span>
+				<div className={'single__movie__placeholder__data'}>
+					<span className="text placeholder placeholder-wave bg-warning"></span>
+					<span className="text placeholder placeholder-wave bg-warning"></span>
+					<span className="text placeholder placeholder-wave bg-warning"></span>
+					<span className="text placeholder placeholder-wave bg-warning"></span>
+					<span className="text placeholder placeholder-wave bg-warning"></span>
+					<span className="text placeholder placeholder-wave bg-warning"></span>
+				</div>
+			</div>
+			<div className={'single__movie__placeholder__info'}>
+				<span className="heading placeholder placeholder-wave bg-warning"></span>
+				<span className="text placeholder placeholder-wave bg-warning"></span>
+				<span className="text placeholder placeholder-wave bg-warning"></span>
+				<span className="text placeholder placeholder-wave bg-warning"></span>
+				<span className="text placeholder placeholder-wave bg-warning"></span>
+				<span className="text placeholder placeholder-wave bg-warning"></span>
+				<span className="text placeholder placeholder-wave bg-warning"></span>
+			</div>
+		</>
+	)
+}
+
+export default C_MoviePage_Placeholder
--- src/pages/Single_Movie.tsx
+++ src/pages/Single_Movie.tsx
@@ -1,128 +1,94 @@
-import {Link, useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 // hooks
 import useGetMovie from "../hooks/useGetMovie.ts";
 // Types
 import {MovieHistory} from "../types/index.types.ts";
 // components
 import C_ErrorHandle from "../components/C_ErrorHandle.tsx";
+import C_MovieScroll from "../components/C_MovieScroll.tsx";
+import C_PersonScroll from "../components/C_PersonScroll.tsx";
+import C_MoviePage_Placeholder from "../components/C_MoviePage_Placeholder.tsx";
 
 const Single_Movie = () => {
-    const {id} = useParams()
-    const movieId = Number(id)
-
-    const {
-        data,
-        isLoading,
-        isSuccess,
-        isError,
-    } = useGetMovie(movieId)
-
-    if (isSuccess && data) {
-        const movieHistory = window.localStorage.getItem('movieHistory') ?? '[]'
-        const movieList = JSON.parse(movieHistory)
-
-        if (!movieList.some((obj: MovieHistory) => obj.id === movieId)) {
-            window.localStorage.setItem('movieHistory', JSON.stringify([{
-                id: movieId,
-                title: data.title,
-                poster_path: data.poster_path
-            }, ...movieList]))
-        }
-    }
-
-    return (
-        <div className={'body'}>
-            {isLoading && (
-                <>
-                    <div className={'single__movie__placeholder__wrap'}>
-                        <span className="image placeholder placeholder-wave bg-warning"></span>
-                        <div className={'single__movie__placeholder__data'}>
-                            <span className="text placeholder placeholder-wave bg-warning"></span>
-                            <span className="text placeholder placeholder-wave bg-warning"></span>
-                            <span className="text placeholder placeholder-wave bg-warning"></span>
-                            <span className="text placeholder placeholder-wave bg-warning"></span>
-                            <span className="text placeholder placeholder-wave bg-warning"></span>
-                            <span className="text placeholder placeholder-wave bg-warning"></span>
-                        </div>
-                    </div>
-                    <div className={'single__movie__placeholder__info'}>
-                        <span className="heading placeholder placeholder-wave bg-warning"></span>
-                        <span className="text placeholder placeholder-wave bg-warning"></span>
-                        <span className="text placeholder placeholder-wave bg-warning"></span>
-                        <span className="text placeholder placeholder-wave bg-warning"></span>
-                        <span className="text placeholder placeholder-wave bg-warning"></span>
-                        <span className="text placeholder placeholder-wave bg-warning"></span>
-                        <span className="text placeholder placeholder-wave bg-warning"></span>
-                    </div>
-                </>
-            )}
-
-
-            {isSuccess && data ? (
-                <>
-                    <div className={'single__movie__wrap'}>
-                        <div className={'single__movie__header'}
-                             style={{backgroundImage: `url('https://image.tmdb.org/t/p/original${data.backdrop_path}')`}}>
-
-                        </div>
-                        <div className={'single__movie__data'}>
-                            <div>
-                                <img alt={data.title} className={'single__movie__cover'}
-                                     src={`https://image.tmdb.org/t/p/w200${data.poster_path}`}/>
-                            </div>
-                            <div>
-                                <p>Status: {data.status}</p>
-                                <p>Release date: {data.release_date}</p>
-                                <p>
-                                    {data.credits.crew.filter(c => c.job === "Director").length > 1 ? 'Directors: ' : 'Director: '}
-                                    {data.credits.crew.filter(c => c.job === "Director").map(c =>
-                                        <span key={c.id}> {c.name} </span>)}
-                                </p>
-                                <p>Runtime: {data.runtime} min</p>
-                                <p>Budget: $ {new Intl.NumberFormat('sv-Se').format(data.budget)}</p>
-                                <p>Revenue: $ {new Intl.NumberFormat('sv-SE').format(data.revenue)}</p>
-                            </div>
-                        </div>
-
-                        <div className={'single__movie__info'}>
-                            <h2 className={'single__movie__title'}>{data.title} <span>{data.tagline}</span></h2>
-                            <p className={'single__movie__overview'}>{data.overview}</p>
-                        </div>
-
-                        <div>
-                            <h3>Cast in {data.title}</h3>
-
-                            <div className={'single__movie__cast'}>
-                                {data.credits.cast.slice(0, 20).map(c => (
-                                    <Link to={`/person/${c.id}`} key={c.id}>
-                                        <img key={c.id}
-                                             src={c.profile_path === null ? `https://placehold.co/200x300/212529/e5a00d?text=!\\nimage\\nmissing&font=montserrat` : `https://image.tmdb.org/t/p/w200${c.profile_path}`}
-                                             alt={c.name}/>
-                                        <p>{c.name} as {c.character}</p>
-                                    </Link>
-                                ))}
-                            </div>
-
-                            <h3> Similar (-ish!) movies as {data.title}</h3>
-                            <div className={'single__movie__similar'}>
-                                {data.similar.results.sort((a, b) => b.popularity - a.popularity).map(c => (
-                                    <Link to={`/movie/${c.id}`} key={c.id}>
-                                        <img key={c.id}
-                                             src={c.poster_path === null ? `https://placehold.co/200x300/212529/e5a00d?text=!\\nimage\\nmissing&font=montserrat` : `https://image.tmdb.org/t/p/w200${c.poster_path}`}
-                                             alt={c.title}/>
-                                        <p>{c.title}</p>
-                                    </Link>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </>
-            ) : null}
-            {isError ? (
-                <C_ErrorHandle variant={'danger'} msg={'Something went wrong when fetching info about this movie.'}/>
-            ) : null}
-        </div>
-    )
+	const {id} = useParams()
+	const movieId = Number(id)
+	
+	const {
+		data,
+		isLoading,
+		isSuccess,
+		isError,
+	} = useGetMovie(movieId)
+	
+	if (isSuccess && data) {
+		const movieHistory = window.localStorage.getItem('movieHistory') ?? '[]'
+		const movieList = JSON.parse(movieHistory)
+		
+		if (!movieList.some((obj: MovieHistory) => obj.id === movieId)) {
+			window.localStorage.setItem('movieHistory', JSON.stringify([{
+				id: movieId,
+				title: data.title,
+				poster_path: data.poster_path
+			}, ...movieList]))
+		}
+	}
+	
+	return (
+		<div className={'body'}>
+			{isLoading && <C_MoviePage_Placeholder/>}
+			
+			{isSuccess && data ? (
+				<>
+					<div className={'single__movie__wrap'}>
+						<div className={'single__movie__header'} style={{backgroundImage: `url('https://image.tmdb.org/t/p/original${data.backdrop_path}')`}}>
+						
+						</div>
+						<div className={'single__movie__data'}>
+							<div>
+								<img alt={data.title} className={'single__movie__cover'} src={`https://image.tmdb.org/t/p/w200${data.poster_path}`}/>
+							</div>
+							<div>
+								<p>Status: {data.status}</p>
+								<p>Release date: {data.release_date}</p>
+								<p>
+									{data.credits.crew.filter(c => c.job === "Director").length > 1 ? 'Directors: ' : 'Director: '}
+									{data.credits.crew.filter(c => c.job === "Director").map(c =>
+										<span key={c.id}> {c.name} </span>)}
+								</p>
+								<p>Runtime: {data.runtime} min</p>
+								<p>Budget: $ {new Intl.NumberFormat('sv-Se').format(data.budget)}</p>
+								<p>Revenue: $ {new Intl.NumberFormat('sv-SE').format(data.revenue)}</p>
+							</div>
+						</div>
+						
+						<div className={'single__movie__info'}>
+							<h2 className={'single__movie__title'}>{data.title} <span>{data.tagline}</span></h2>
+							<p className={'single__movie__overview'}>{data.overview}</p>
+						</div>
+						
+						{isSuccess && data.credits && data.similar ? (
+							
+							<div>
+								<h3>Cast in {data.title}</h3>
+								<div className={'single__movie__cast'}>
+									<C_PersonScroll data={data.credits}/>
+								</div>
+								<h3> Similar (-ish!) movies as {data.title}</h3>
+								<div className={'single__movie__similar'}>
+									<C_MovieScroll data={data.similar}/>
+								</div>
+							</div>
+						) : null}
+					</div>
+				</>
+			) : null}
+			
+			{isError ? (
+				<C_ErrorHandle variant={'danger'} msg={'Something went wrong when fetching info about this movie.'}/>
+			) : null}
+		
+		</div>
+	)
 }
 
 export default Single_Movie
